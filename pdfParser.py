90ecb5cfb83c8312f661c63cb023c6c1d8b1e538
vish-nu-ram <vishnu.ram@hotmail.com>
2022-06-29 23:42:52 +0100

    Adding PDF Parser

---
+++ pdfParser.py
@@ -0,0 +1,25 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.layout import LAParams
+import io
+
+
+def pdfparser(path):
+    """ Function that takes a path of a PDF file as input and extracts the text within the file as a string"""
+
+    fp = open(path, 'rb')
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    # Create a PDF interpreter object.
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    # Process each page contained in the document.
+
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+        pdf_text = retstr.getvalue()
+
+    return pdf_text

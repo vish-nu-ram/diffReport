86629dd1ccb85b59991e9fb9794ba03586a232db
vish-nu-ram <vishnu.ram@hotmail.com>
2022-06-29 23:42:42 +0100

    Adding Fuzzy Compare

---
+++ fuzzyCompare.py
@@ -0,0 +1,92 @@
+from fuzzywuzzy import fuzz
+
+
+def ratio(t1, t2, ratio):
+    switcher = {
+        "qRatio": qRatio,
+        "qRatio": wRatio,
+        "partialRatio": partialRatio,
+        "tokenSetRatio": tokenSetRatio,
+        "tokenSortRatio": tokenSortRatio,
+        "partialTokenSortRatio": partialTokenSortRatio,
+    }
+    result = switcher[ratio](t1, t2)
+    return result
+
+
+def partialRatio(t1, t2):
+    """
+        :param t1: Text string 1
+        :param t2: Text String 2
+        :return: Returns the Partial Ratio score etween the two given text
+
+        Function to calculate the ratio of the most similar substring as a number between 0 and 100.
+
+        Example : "" and "" returns a score of :
+    """
+    return fuzz.partial_ratio(t1, t2)
+
+
+def tokenSetRatio(t1, t2):
+    """
+        :param t1: Text string 1
+        :param t2: Text String 2
+        :return: Returns the Token Set Ratio score between the two given text
+
+        Function to calculate the ratio of??????????????????????????
+
+        Example : "" and "" returns a score of :
+    """
+    return fuzz.token_set_ratio(t1, t2)
+
+
+def tokenSortRatio(t1, t2):
+    """
+        :param t1: Text string 1
+        :param t2: Text String 2
+        :return: Returns the Token Sort Ratio score between the two given text
+
+        Function to calculate the ratio of??????????????????????????
+
+        Example : "" and "" returns a score of :
+    """
+    return fuzz.token_sort_ratio(t1, t2)
+
+
+def partialTokenSortRatio(t1, t2):
+    """
+        :param t1: Text string 1
+        :param t2: Text String 2
+        :return: Returns the Partial Token Sort Ratio score between the two given text
+
+        Function to calculate the ratio of??????????????????????????
+
+        Example : "" and "" returns a score of :
+    """
+    return fuzz.partial_token_sort_ratio(t1, t2)
+
+
+def qRatio(t1, t2):
+    """
+        :param t1: Text string 1
+        :param t2: Text String 2
+        :return: Returns the Q Ratio score between the two given text
+
+        Function to calculate the ratio of??????????????????????????
+
+        Example : "" and "" returns a score of :
+    """
+    return fuzz.QRatio(t1, t2)
+
+
+def wRatio(t1, t2):
+    """
+        :param t1: Text string 1
+        :param t2: Text String 2
+        :return: Returns the W Ratio score between the two given text
+
+        Function to calculate the ratio of??????????????????????????
+
+        Example : "" and "" returns a score of :
+    """
+    return fuzz.WRatio(t1, t2)

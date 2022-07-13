42e4dc2dac5dbc5720731498887716e8276b85f4
vish-nu-ram <vishnu.ram@hotmail.com>
2022-07-13 15:27:02 +0100

    0.0.16

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name='diffReport',
-    version=' 0.0.14',
+    version=' 0.0.16',
     description='File comparison and report differences',
     long_description=long_description,
     long_description_content_type="text/markdown",
